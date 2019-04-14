47ecdc53b3513631f1cac9185fce77ab443ba484
sarahnfdez <44241502+sarahnfdez@users.noreply.github.com>
2019-04-14 13:06:37 -0400

    update miniproject4.py

--- miniproject4.py
+++ miniproject4.py
@@ -14,9 +14,9 @@ class corpus_classifier:
         return None
     
     def load_training_data(self):
-        X_training = [550152]
+        X_training = [None]*550152
         x_index = 0
-        y_training = [550152]
+        y_training = [None]*550152
         y_index = 0
         
         with open("snli_1.0_train.txt") as f:
@@ -43,9 +43,9 @@ class corpus_classifier:
         return X_training, y_training
     
     def load_validation_data(self):
-        X_val = [550152]
+        X_val = [None]*10000
         x_index = 0
-        y_val = [550152]
+        y_val = [None]*10000
         y_index = 0
         
         with open("snli_1.0_dev.txt") as f:
@@ -53,13 +53,13 @@ class corpus_classifier:
             d = list(reader)
         df = d[1:]
         
-        #loads x training
+        #loads x validation
         for arr in df:
             sentences = arr[5:7]
             X_val[x_index] = sentences
             x_index += 1
             
-        #Loads y training
+        #Loads y validation
         for arr in df:
             element = arr[0]
             if(element != 'entailment'):
@@ -72,9 +72,9 @@ class corpus_classifier:
         return X_val, y_val
     
     def load_test_data(self):
-        X_test = [550152]
+        X_test = [None]*10000
         x_index = 0
-        y_test = [550152]
+        y_test = [None]*10000
         y_index = 0
         
         with open("snli_1.0_test.txt") as f:
@@ -82,13 +82,13 @@ class corpus_classifier:
             d = list(reader)
         df = d[1:]
         
-        #loads x training
+        #loads x test
         for arr in df:
             sentences = arr[5:7]
             X_test[x_index] = sentences
             x_index += 1
             
-        #Loads y training
+        #Loads y test
         for arr in df:
             element = arr[0]
             if(element != 'entailment'):
@@ -100,14 +100,15 @@ class corpus_classifier:
         
         return X_test, y_test
     
-    def edit_distance_preprocess(self, x):
+    def edit_distance_preprocess(self, x, size):
         stop_words = set(stopwords.words('english'))
-        preprocessed1 = [550152]
+        preprocessed1 = [None]*size
         pi = 0
         for pair in x:
-            finalpair = [2]
+            finalpair = [None]*2
             fi = 0
             for sentence in pair:
+             #   print(sentence)
                 sentence = sentence.lower()
                 sentence = word_tokenize(sentence)
                 sentence = [w for w in sentence if not w in stop_words]
@@ -116,10 +117,10 @@ class corpus_classifier:
             preprocessed1[pi] = finalpair
             pi += 1
             
-        preprocessed2 = [550152]
+        preprocessed2 = [None]*550152
         pi = 0
         for pair in preprocessed1:
-            finalpair= [2]
+            finalpair= [None]*2
             fi = 0
             for sentence in pair:
                 final = ''
@@ -131,14 +132,14 @@ class corpus_classifier:
                         final = final[:-1]
                 finalpair[fi] = final
                 fi += 1
-            preprocessed2[finalpair]
+            preprocessed2[pi] = finalpair
             pi += 1
         
         return preprocessed2
     
     def jaro_winkler_classifier(self, x_train, y_train, x_test):
-        x_dist = [550152]
-        y_test = [10000]
+        x_dist = [None]*550152
+        y_test = [None]*10000
         
         xd_index = 0
         for pair in x_train:
@@ -162,8 +163,8 @@ class corpus_classifier:
         return y_test
     
     def levenshtein_classifier(self, x_train, y_train, x_test):
-        x_dist = [550152]
-        y_test = [10000]
+        x_dist = [None]*550152
+        y_test = [None]*10000
         
         xd_index = 0
         for pair in x_train:
@@ -187,8 +188,8 @@ class corpus_classifier:
         return y_test
     
     def hamming_classifier(self, x_train, y_train, x_test):
-        x_dist = [550152]
-        y_test = [10000]
+        x_dist = [None]*550152
+        y_test = [None]*10000
         
         xd_index = 0
         for pair in x_train:
@@ -210,6 +211,36 @@ class corpus_classifier:
         
         y_test = y_test[1:]
         return y_test
+    
+    def error_calculator(self, y_test, y_val):
+        tn = 0
+        tp = 0
+        fn = 0
+        fp = 0
+        yv_index = 0
+        for output in y_test:
+            if(output == 1 and y_val[yv_index] == 1):
+                tp += 1
+                yv_index += 1
+            elif(output == 0 and y_val[yv_index] == 0):
+                tn += 1
+                yv_index += 1
+            elif(output == 1 and y_val[yv_index] == 0):
+                fp += 1
+                yv_index += 1
+            elif(output == 0 and y_val[yv_index] == 1):
+                fn += 1
+                yv_index += 1
+        accuracy = ((tp+tn)/(tp+tn+fp+fn))*100
+        precision = (tp/(tp+fp))*100
+        recall = (tp/(tp+fn))*100
+        print("The accuracy: %i%" % accuracy)
+        print("The precision: %i%" % precision)
+        print("The recall: %i%" % recall)
+        return accuracy, precision, recall
+        
+        
+            
    
 #############################################################
         
@@ -217,8 +248,8 @@ test = corpus_classifier()
 x_train, y_train = test.load_training_data()
 x_val1, y_val = test.load_validation_data()
 
-x_train_final = test.edit_distance_preprocess(x_train)
+x_train_final = test.edit_distance_preprocess(x_train, 550152)
 
-x_val_final = test.edit_distance_preprocess(x_val1)
+x_val_final = test.edit_distance_preprocess(x_val1, 10000)
 
 y_test = test.jaro_winkler_classifier(x_train_final, y_train, x_val_final)
